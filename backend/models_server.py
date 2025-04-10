b34fadfd0686691ea9787e798d3ac627784628fc
vivi2004 <vivekpurbey586@gmail.com>
2025-04-11 00:59:20 +0530

    All issues are resolved

--- backend/models_server.py
+++ backend/models_server.py
@@ -5,7 +5,12 @@ import joblib
 app = Flask(__name__)
 CORS(app, resources={
     r"/predict": {
-        "origins": ["http://localhost:5500", "http://127.0.0.1:5500"],
+        "origins": [
+            "http://localhost:5500",
+            "http://127.0.0.1:5500",
+            "http://localhost:5501",
+            "http://127.0.0.1:5501"
+        ],
         "methods": ["POST"],
         "allow_headers": ["Content-Type"]
     }
